a336c2eef47c305e417b1787d8cc1241e31cc288
Rajbarvadiya <raj.barvadiya@eximietas.design>
2024-02-22 23:27:34 +0530

    change in repo2

--- rtl.v
+++ rtl.v
@@ -7,11 +7,11 @@ output sum,carry; //output port
 wire [2:0]p; //take as wire
 //gate level modeling
 xor x1(sum,a,b,c);
-and a1(p[0],a,b);
-and a2(p[1],b,c);
-and a3(p[2],a,c);
+and c1(p[0],a,b);
+and c2(p[1],b,c);
+and c3(p[2],a,c);
 or o1(carry,p[0],p[1],p[2]);
 //data flow level modeling
-/* assign sum = a ^ b ^ c;
+assign add = a ^ b ^ c;
 assign carry = a&b | a&c | b&c; */
 endmodule
